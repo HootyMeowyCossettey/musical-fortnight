d9ed0353f20bf869af86ccb6e66780741ad18c40
HootyMeowyCossettey <91523274+HootyMeowyCossettey@users.noreply.github.com>
2021-10-20 18:31:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+canvas=document.getElementById("my_canvas");
+ctx=canvas.getContext("2d");
+var colour=document.getElementById("color").value;
+color="colour";
+ctx.beginPath();
+ctx.strokeStyle=colour;
+ctx.lineWidth=4;
+ctx.arc(200,200,40,0,2*Math.PI);
+ctx.stroke();
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e){
+
+   
+    mouse_x=e.clientX-canvas.offsetLeft;
+    mouse_y=e.clientY-canvas.offsetTop;
+    console.log("X="+mouse_x+",Y="+mouse_y);
+    circle(mouse_x,mouse_y);
+    }
+function circle(mouse_X,mouse_Y){
+    ctx.beginPath();
+    ctx.strokeStyle=colour;
+    ctx.lineWidth=4;
+    ctx.arc(mouse_X,mouse_Y,40,0,2*Math.PI);
+    ctx.stroke(); 
+    canvas.addEventListener("mousedown",my_mousedown);  
+}
